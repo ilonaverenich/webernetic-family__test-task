f7b21d1350e5641a77861f043cc6218cbd4c0fd7
Ilona <Ilonaverenich@tut.by>
2024-07-13 17:53:13 +0300

    adding modal window structure to js file

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,23 @@ const modalBody = document.getElementById('modal-body');
 openModalBtn.onclick = function () {
   modal.style.display = 'flex';
   modalBody.innerHTML = `
-        <div>Модальное окно</div>
-        
-    `;
+        <h2 class="modal__title">Войти в систему</h2>
+
+        <div class="form-group-input">
+            <input type="text" placeholder="Email/Телефон" id="email" name="email" required>
+            <input type="password" placeholder="Пароль" id="password" name="password" required>
+        </div>
+
+        <div class="form-group-checkbox">
+            <input type="checkbox" id="remember" name="remember">
+            <label for="remember">Запомнить пароль</label>
+        </div>
+        <a href="#" class="restore">Восстановить</a>
+        <div class="form-group-button">
+            <button class="submit" type="submit">Войти</button>
+            <button class="registerBtn" type="button" id="registerBtn">Зарегистрироваться</button>
+        </div>
+`;
 };
 
 closeBtn.onclick = function () {
